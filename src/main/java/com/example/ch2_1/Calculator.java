71e94d6a88b999be498c997b6a05487b41c59166
Bindu <bindukrishnamurthyid@gmail.com>
2026-05-21 10:21:22 +0530

    message committed

---
+++ src/main/java/com/example/ch2_1/Calculator.java
@@ -0,0 +1,14 @@
+package com.example.ch2_1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		int sum = a + b;
+
+		System.out.println(sum);
+	}
+
+}
